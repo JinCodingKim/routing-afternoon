1c038a510bdb2e9e8d19e457136193448e58fa4a
unknown <jinyeob.kim.0818@gmail.com>
2017-12-12 14:38:24 -0600

    Finished Product

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import routes from "./routes.js";
+import { Link } from "react-router-dom";
 
 export default class App extends Component {
-
-    render() {
-        return (
-           <div>
-               <nav className='nav'>
-                   <div>WestSide University</div> 
-                   <div className='link-wrap'>
-                        <div className='links'>Home</div>
-                        <div className='links'>About</div> 
-                   </div>  
-               </nav>
-            
-           </div> 
-        )
-    }
-
+  render() {
+    return (
+      <div>
+        <nav className="nav">
+          <div>WestSide University</div>
+          <div className="link-wrap">
+            <Link className="links" to="/">
+              Home
+            </Link>
+            <Link className="links" to="/about">
+              About
+            </Link>
+            <Link className="links" to="/classlist">
+              Class List
+            </Link>
+          </div>
+        </nav>
+        {routes}
+      </div>
+    );
+  }
 }
